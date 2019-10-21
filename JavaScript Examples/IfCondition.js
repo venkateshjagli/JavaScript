df22a53af8b03346dd28e230f169e31ccc8a770a
venkateshjagli <venkateshjagli7@gmail.com>
2019-10-21 21:23:30 +0530

    JavaScript Docu, if n or example
    JavaScript Docu, if n or example updated

---
+++ JavaScript Examples/IfCondition.js
@@ -0,0 +1,27 @@
+//If Program
+let age = 28;
+if(age < 30){
+    console.log("Young Men");
+}
+//else Program
+let age1 = 30;
+if(age1 < 28){
+    console.log("Young Men");
+}
+else{
+    console.log("Gentleman");
+}
+
+//else If Condition
+let age2 = 50;
+if(age2 < 35){
+    console.log("Young Men");
+}
+else if(age2 < 40){
+    console.log("Gentleman");
+}
+else {
+    console.log("Old Man");
+}
+
+
---
+++ JavaScript Examples/OR_operator.js
@@ -0,0 +1,4 @@
+let age = 31;
+if(age < 25 || age > 30){
+    console.log(" He Not Selected this Exam Age More than 30 years");
+}
