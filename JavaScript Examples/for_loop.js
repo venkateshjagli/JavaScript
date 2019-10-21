536b085deb8314bc0a8b3604cd4b07369ad18223
venkateshjagli <venkateshjagli7@gmail.com>
2019-10-21 23:11:19 +0530

    js for loop
    js for loop updated

--- JavaScript Examples/for_loop.js
+++ JavaScript Examples/for_loop.js
@@ -3,3 +3,27 @@ for (let index = 0; index < 3; index++) {
  console.log(element);
     
 }
+
+var name = ["Venky", "Chandu", "Naresh", "Thimppa"];
+var text = " ";
+var i ;
+for(i=0; i<name.length; i++){
+text = name[i] ;
+console.log(text);
+}
+ 
+ 
+ var text;
+ var i;
+ for(i=0; i<5; i++){
+	 text = 5*i;
+ console.log(text);
+ }
+ 
+ 
+ var text;
+ var i;
+ for(i=2; i<10; i+=2){
+	 text = i;
+ console.log(text);
+ }
